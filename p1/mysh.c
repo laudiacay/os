bbbbcfa4a23fd7a9a9e0e345a9dce5338ab60674
Claude Richoux <laudecay@gmail.com>
2019-04-24 23:27:25 +0000

    FUCK

--- p1/mysh.c
+++ p1/mysh.c
@@ -44,8 +44,7 @@ int main(int argc, char* argv[]) {
     // handle bad input
     if (argc > 2) {
         do_error();
-        printf("yeet\n");
-        return 1;
+        exit(1);
     }
 
     // space to store command, plus null terminator
@@ -58,17 +57,12 @@ int main(int argc, char* argv[]) {
         FILE* fp = fopen(batch_filename, mode);
         if (!fp) {
             do_error();
-            printf("yeeeeet\n");
-            return 1;
+            exit(1);
         }
-        while (!feof(fp)) {
-            one_iter(cmd_input_buf, cmds, 1, fp);
-        }
+        while (!feof(fp)) one_iter(cmd_input_buf, cmds, 1, fp);
         fclose(fp);
     } else {
-        while (1) {
-            one_iter(cmd_input_buf, cmds, 0, NULL);
-        }
+        while (1) one_iter(cmd_input_buf, cmds, 0, NULL);
     }
 
     return 0;
