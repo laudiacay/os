267493cb297379bf227582930edad02c79ea762f
Claude Richoux <laudecay@gmail.com>
2019-04-24 23:28:18 +0000

    FUCK

--- p1/mysh.c
+++ p1/mysh.c
@@ -44,6 +44,7 @@ int main(int argc, char* argv[]) {
     // handle bad input
     if (argc > 2) {
         do_error();
+        printf("why aren't you erroring right you bastard\n");
         exit(1);
     }
 
